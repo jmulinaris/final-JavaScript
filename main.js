e379a6317d4290881da558706ac59345dee2f650
julimulinaris <juli.mulinaris@gmail.com>
2022-04-04 16:46:36 -0300

    primera entrega del proyecto

--- main.js
+++ main.js
@@ -29,53 +29,63 @@ console.log(productos)
 function agregarAlCarrito (){
     do {
         let producto = parseInt (prompt("Qué necesitás comprar?:\n 1- Promo Fernet\n 2- Promo Campari\n 3- Promo Gancia"));
-        let cantidad = prompt("Ingresá la cantidad");
-        switch (producto){
-            case 1:
-                if (cantidad > productos[0].stock){
-                    alert("Lo sentimos, no tenemos stock suficiente" + "\nEl stock disponible es de " + productos[0].stock);
+        let cantidad = Number(prompt("Ingresá la cantidad"));
+            switch (producto){
+                case 1:
+                    if (cantidad > productos[0].stock){
+                        alert("Lo sentimos, no tenemos stock suficiente" + "\nEl stock disponible es de " + productos[0].stock);
+                        precio = 0;
+                        cantidad = 0;
+                    } else if (cantidad ===0){
+                        alert("No se agregó ningún producto, intente nuevamente")
+                    }
+                    else {
+                        alert("Agregaste al carrito " + cantidad + " unidades de " + productos[0].nombre);
+                        precio = productos[0].precio;
+                        carritoDeCompras.push(new Pedido (productos[0].nombre, precio, cantidad));
+                    };
+                    break;
+                case 2:
+                    if (cantidad > productos[1].stock){
+                        alert("Lo sentimos, no tenemos stock suficiente" + "\nEl stock disponible es de " + productos[1].stock);
+                        precio = 0;
+                        cantidad = 0;
+                    } else if (cantidad ===0){
+                        alert("No se agregó ningún producto, intente nuevamente")
+                    }else {
+                        alert("Agregaste al carrito " + cantidad + " unidades de " + productos[1].nombre);
+                        precio = productos[1].precio;
+                        carritoDeCompras.push(new Pedido (productos[1].nombre, precio, cantidad));
+                    }
+                    break;
+                case 3:
+                    if (cantidad > productos[2].stock){
+                        alert("Lo sentimos, no tenemos stock suficiente" + "\nEl stock disponible es de " + productos[2].stock);
+                        precio = 0;
+                        cantidad = 0;
+                    } else if (cantidad ===0){
+                        alert("No se agregó ningún producto, intente nuevamente")
+                    }
+                    else {
+                        alert("Agregaste al carrito " + cantidad + " unidades de " + productos[2].nombre);
+                        precio = productos[2].precio;
+                        carritoDeCompras.push(new Pedido (productos[2].nombre, precio, cantidad));
+                    }
+                    break;
+                default:
+                    alert("Algunos de los datos ingresados no son correctos");
                     precio = 0;
                     cantidad = 0;
-                } else {
-                    alert("Agregaste al carrito " + cantidad + " unidades de " + productos[0].nombre);
-                    precio = productos[0].precio;
-                    carritoDeCompras.push(new Pedido (productos[0].nombre, precio, cantidad));
-                };
-                break;
-            case 2:
-                if (cantidad > productos[1].stock){
-                    alert("Lo sentimos, no tenemos stock suficiente" + "\nEl stock disponible es de " + productos[1].stock);
-                    precio = 0;
-                    cantidad = 0;
-                } else {
-                    alert("Agregaste al carrito " + cantidad + " unidades de " + productos[1].nombre);
-                    precio = productos[1].precio;
-                    carritoDeCompras.push(new Pedido (productos[1].nombre, precio, cantidad));
-                }
-                break;
-            case 3:
-                if (cantidad > productos[2].stock){
-                    alert("Lo sentimos, no tenemos stock suficiente" + "\nEl stock disponible es de " + productos[2].stock);
-                    precio = 0;
-                    cantidad = 0;
-                } else {
-                    alert("Agregaste al carrito " + cantidad + " unidades de " + productos[2].nombre);
-                    precio = productos[2].precio;
-                    carritoDeCompras.push(new Pedido (productos[2].nombre, precio, cantidad));
-                }
-                break;
-            default:
-                alert("Algunos de los datos ingresados no son correctos");
-                precio = 0;
-                cantidad = 0;
-        }
-        total = total + precio * cantidad;
-        otroProducto = confirm("¿Querés agregar otro producto?");
+            }
+            total = total + precio * cantidad;
+            otroProducto = confirm("¿Querés agregar otro producto?");
     } while (otroProducto);
     console.log(carritoDeCompras);
     const contadorCarrito = carritoDeCompras.reduce ((acc, el) => acc + el.cantidad, 0);
     const precioTotal = carritoDeCompras.reduce ((acc, el) => acc + (el.cantidad * el.precio), 0);
+    if (precioTotal !=0){
     alert ("La cantidad de productos agregados al carrito es de " + contadorCarrito + "\nTotal: $" + precioTotal)
+    }
     console.log(precioTotal);
 }
 
@@ -93,4 +103,8 @@ function calcularEnvio (total){
 }
 
 agregarAlCarrito();
-calcularEnvio ((total));
+if (total > 0) {
+	calcularEnvio(total);
+} else {
+	alert('No has agregado nada al carrito, vuelve pronto.');
+}
