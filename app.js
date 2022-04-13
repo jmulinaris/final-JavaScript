64f5c110183f0c461abbda6bc93313678d96041b
julimulinaris <juli.mulinaris@gmail.com>
2022-04-13 15:46:12 -0300

    DOM y eventos

---
+++ app.js
@@ -0,0 +1,23 @@
+import {productos} from "./stock.js";
+
+const mostrarProductos = (productos) => {
+    const contenedorProductos = document.getElementById("productos");
+    productos.forEach(producto => {
+        const div = document.createElement("div");
+        div.classList.add("row");
+        div.innerHTML += `
+            <div class="card col" style="width: 18rem;">
+                <img src=${producto.img}>
+            <div class="card-body">
+                <h5 class="card-title">${producto.nombre}</h5>
+                <p class="card-text">${producto.desc}</p>
+                <p><strong>$${producto.precio}</strong></p>
+                <a href="#" class="btn btn-dark">COMPRAR</a>
+            </div>
+            </div>
+            `
+    contenedorProductos.appendChild (div);
+    });
+}
+
+mostrarProductos(productos);
---
+++ modal.js
@@ -0,0 +1,18 @@
+// const modalContenedor = document.querySelector(".modal-contenedor");
+// const abrirCarrito = document.getElementsByClassName("carrito");
+
+// abrirCarrito.addEventListener("click", () => {
+//     modalContenedor.classList.toggle ("modal");
+// } )
+
+const open = document.getElementById("open");
+const modal_container = document.getElementById("modal_container");
+const close = document.getElementById("close");
+
+open.addEventListener('click', () => {
+    modal_container.classList.add("show");
+});
+
+close.addEventListener('click', () => {
+    modal_container.classList.remove("show");
+});
